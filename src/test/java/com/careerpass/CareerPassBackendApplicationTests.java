1f5bde4fd77e1c0ca2e279287eaf296b713e8772
Josh0511 <wh5054@naver.com>
2025-09-22 16:52:22 +0900

    환경 설정

---
+++ src/test/java/com/careerpass/CareerPassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
